8c1e8b7c2b09260742852dd50a95faa31f754ba1
Jasper Chapman-Black <jasperchapmanblack@gmail.com>
2016-01-24 16:58:04 -0500

    added debugLog(Unit) method

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -1,4 +1,6 @@
 #include "Logging.hpp"
+#include "Unit.hpp"
+#include "UnitTemplate.hpp"
 
 void debugLog(std::string s){
 	std::cout<<s<<std::endl;
@@ -6,4 +8,10 @@ void debugLog(std::string s){
 
 void debugLog(int s){
 	std::cout<<s<<std::endl;
+}
+
+void debugLog(Unit& u){
+	std::cout<<"Unit Log:"<<std::endl;
+	std::cout<<"  Position: "<<u.xy.first<<","<<u.xy.second<<std::endl;
+	std::cout<<"  HP: "<<u.hp<<"/"<<u.getUnitTemplate().maxHP()<<std::endl;
 }
--- src/Logging.hpp
+++ src/Logging.hpp
@@ -3,5 +3,8 @@
 #include <string>
 #include <iostream>
 
+class Unit;
+
 void debugLog(std::string s);
 void debugLog(int s);
+void debugLog(Unit& u);
