d09735c942debc8547768208cb50c6cd172b0753
Jasper Chapman-Black <jasperchapmanblack@gmail.com>
2016-01-24 17:33:43 -0500

    added debugLog(Weapon&)

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -1,6 +1,7 @@
 #include "Logging.hpp"
 #include "Unit.hpp"
 #include "UnitTemplate.hpp"
+#include "Weapon.hpp"
 
 void debugLog(std::string s){
 	std::cout<<s<<std::endl;
@@ -12,6 +13,14 @@ void debugLog(int s){
 
 void debugLog(Unit& u){
 	std::cout<<"Unit Log:"<<std::endl;
-	std::cout<<"  Position: "<<u.xy.first<<","<<u.xy.second<<std::endl;
-	std::cout<<"  HP: "<<u.hp<<"/"<<u.getUnitTemplate().maxHP()<<std::endl;
+	std::cout<<"  unitID: "<<u.unitID<<std::endl;
+	std::cout<<"  teamID: "<<u.teamID<<std::endl;
+	std::cout<<"  xy: "<<u.xy.first<<","<<u.xy.second<<std::endl;
+	std::cout<<"  hp: "<<u.hp<<std::endl;
+}
+
+void debugLog(Weapon& w){
+	std::cout<<"Weapon Log:"<<std::endl;
+	std::cout<<"  ticksUntilCanFire: "<<w.ticksUntilCanFire<<std::endl;
+	debugLog(w.owner);
 }
--- src/Logging.hpp
+++ src/Logging.hpp
@@ -4,7 +4,9 @@
 #include <iostream>
 
 class Unit;
+class Weapon;
 
 void debugLog(std::string s);
 void debugLog(int s);
 void debugLog(Unit& u);
+void debugLog(Weapon& w);
