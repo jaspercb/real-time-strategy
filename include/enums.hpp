2348a4f69502342a6a5a7f2d16027d92cf153cb6
Jasper Chapman-Black <jasperchapmanblack@gmail.com>
2016-02-05 13:28:42 -0500

    moved some enums into enums.hpp to resolve circular dependencies

---
+++ include/enums.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+enum TeamColor{
+	/*	These values are set to match with the colors in resources/graphics/team-colors.png
+	*/
+	COLOR_PINK=0,
+	COLOR_RED=1,
+	COLOR_BLUE=2,
+	COLOR_TEAL=3,
+	COLOR_PURPLE=4,
+	COLOR_ORANGE=5,
+	COLOR_BROWN=6,
+	COLOR_WHITE=7,
+	COLOR_YELLOW=8,
+	COLOR_GREEN=9,
+	COLOR_TAN=10,
+	COLOR_PALE=11,
+	COLOR_IRIS=12,
+	COLOR_OLIVE=13,
+	COLOR_METAL=14,
+	COLOR_CYAN=15
+
+};
