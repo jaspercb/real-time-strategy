a4624b18aa13449d31ec0117c98551211e559586
Jasper Chapman-Black <jasperchapmanblack@gmail.com>
2016-02-27 15:21:20 -0500

    added sdlTools for misc. tools for interfacing with SDL

---
+++ include/sdlTools.hpp
@@ -0,0 +1,6 @@
+#include <SDL2/SDL.h>
+
+#include "typedefs.hpp"
+
+void renderRectBorder(SDL_Renderer* renderer, Coordinate a, Coordinate b, SDL_Color color);
+void renderLine(SDL_Renderer* renderer, Coordinate a, Coordinate b, SDL_Color color);
---
+++ src/sdlTools.cpp
@@ -0,0 +1,22 @@
+#include "sdlTools.hpp"
+
+void renderRectBorder(SDL_Renderer* renderer, Coordinate a, Coordinate b, SDL_Color color) {
+	SDL_Rect drawRect;
+	
+	drawRect.x = a.first;
+	drawRect.y = a.second;
+	drawRect.w = b.first-a.first;
+	drawRect.h = b.second-a.second;
+
+	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+
+	SDL_RenderDrawRect(renderer, &drawRect);
+}
+
+void renderLine(SDL_Renderer* renderer, Coordinate a, Coordinate b, SDL_Color color) {
+	SDL_Rect drawRect;
+	
+	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+
+	SDL_RenderDrawLine(renderer, a.first, a.second, b.first, b.second);
+}
