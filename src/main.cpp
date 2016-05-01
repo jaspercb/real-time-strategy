33f1e1cba7cfcdd52d2da87e35d12a014ee44469
Jasper Chapman-Black <jasperchapmanblack@gmail.com>
2016-05-01 15:27:46 -0700

    cleaned up main.cpp, removed old code, better variable names

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,8 @@
 
 const int SCREEN_WIDTH = 1600;
 const int SCREEN_HEIGHT = 900;
-const int FRAMERATE_SIM = 20; // simulation framerate
-const int FRAMERATE_UI = 3; // UI frames per simulation frame
+const int GAME_TICKS_PER_SECOND = 20; // simulation framerate
+const int UI_FRAMES_PER_GAMETICK = 3; // UI frames per simulation frame
 
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
@@ -36,8 +36,7 @@ bool init() {
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 		success = false;
 	}
-	else
-	{
+	else {
 		//Create window
 		gWindow = SDL_CreateWindow( "real time strategy prototype", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( gWindow == NULL ) {
@@ -90,51 +89,31 @@ int main() {
 		t1->loadUnitTemplate("../conf/units/bomber");
 
 		t2->loadUnitTemplate("../conf/units/ground-dummy");
-		
-		//UnitTemplate& p1 = t1.unitTemplates.begin()->second;
-		//UnitTemplate& p2 = t2.unitTemplates.begin()->second;
 
 		game->createUnit(tID1, "../conf/units/arcology", Coordinate(2*32*PIXEL_WIDTH, 2*32*PIXEL_WIDTH));
-		
+
 		for (int i=5; i<15; i++) {
 			for (int j=5; j<15; j++) {
-				//game->createUnit(tID2, (UnitTemplateID)3, Coordinate(50*i, 50*j));
 				game->createUnit(tID1, "../conf/units/bomber", Coordinate(2500*i, 2500*j));
 			}
 		}
-		
+
 
 		for (int i=5; i<15; i++) {
 			for (int j=5; j<15; j++) {
-				//game->createUnit(tID2, (UnitTemplateID)3, Coordinate(50*i, 50*j));
 				game->createUnit(tID2, "../conf/units/ground-dummy", Coordinate(2500*16 + 2500*i, 2500*16 + 2500*j));
 			}
 		}
 
-		//game->createUnit(tID2, "../conf/units/ground-dummy", Coordinate(0, 0));
-
-		//Command cmd1(CMD_GOTOCOORD);
-		//cmd1.targetID = b;
-
-		//cmd1.queueSetting=QUEUE_OVERWRITE;
-
-		//game->getUnit(a).handleCommand(cmd1);
-
-		//Command cmd2(CMD_GOTOCOORD);
-		//cmd2.targetCoord = Coordinate(300, 300);
-		//cmd2.queueSetting=QUEUE_OVERWRITE;
-
-		//game->getUnit(b).handleCommand(cmd2);
-
 		bool exit;
 
-		for (int i=0; i<2000 && !exit; i++) {
-			for (int j=0; j<FRAMERATE_UI; j++) {
+		while (!exit) {
+			for (int j=0; j<UI_FRAMES_PER_GAMETICK; j++) {
 				exit=gUserInterface->handleInputEvents(); // returns 1 if quit command processed
 
 				gUserInterface->tick();
 				gUserInterface->renderAll( gRenderer );
-				SDL_Delay( 1000/ (FRAMERATE_SIM*FRAMERATE_UI) );
+				SDL_Delay( 1000/ (GAME_TICKS_PER_SECOND*UI_FRAMES_PER_GAMETICK) );
 			}
 			game->tick();
 		}
