55925cfd367d8601b54fc5135c12b5a9bd515b4a
Jasper Chapman-Black <jasperchapmanblack@gmail.com>
2016-05-16 23:09:33 -0700

    probably a good idea to follow the Google C++ style guide

--- include/enums.hpp
+++ include/enums.hpp
@@ -1,88 +1,88 @@
 #pragma once
 
 namespace AnimationState {
-	enum Enum {
-		Idle,
-		Walking,
-		Attacking,
-		Dying
-	};
+enum Enum {
+	Idle,
+	Walking,
+	Attacking,
+	Dying
+};
 };
 
 namespace UpdateStatus {
-	enum Enum {
-		OK,
-		Remove
-	};
+enum Enum {
+	OK,
+	Remove
+};
 };
 
 namespace TeamColor {
-	enum Enum {
-		Null, // for when we don't want to recolor
-		Pink,
-		Red,
-		Blue,
-		Green,
-		Purple,
-		Orange,
-		Brown,
-		Yellow,
-		Teal,
-		Pale,
-		Iris,
-		Olive,
-		num
-	};
+enum Enum {
+	Null, // for when we don't want to recolor
+	Pink,
+	Red,
+	Blue,
+	Green,
+	Purple,
+	Orange,
+	Brown,
+	Yellow,
+	Teal,
+	Pale,
+	Iris,
+	Olive,
+	num
+};
 };
 
 namespace DamageType {
-	enum Enum {
-		Electrical, // good against shields, real bad against armor
-		Thermal,    // mediocre against shields and armor
-		Kinetic,    // bad against shields, pretty good against armor
-		Explosive,  // real bad against shields, great against armor
-		num
-	};
+enum Enum {
+	Electrical, // good against shields, real bad against armor
+	Thermal,    // mediocre against shields and armor
+	Kinetic,    // bad against shields, pretty good against armor
+	Explosive,  // real bad against shields, great against armor
+	num
+};
 };
 
 namespace ResourceType {
-	enum Enum {
-		Metal,   // consumed on build order
-		Crystal, // consumed on build order
-		Supply,  // You get supply back when a unit is destroyed
-		num
-	};
+enum Enum {
+	Metal,   // consumed on build order
+	Crystal, // consumed on build order
+	Supply,  // You get supply back when a unit is destroyed
+	num
+};
 };
 
 namespace CommandType {
-	enum Enum {
-		Idle, 
-		Gotocoord,
-		Attack,
-		Attackmove,
-		Halt,
-		Build
-	};
+enum Enum {
+	Idle, 
+	Gotocoord,
+	Attack,
+	Attackmove,
+	Halt,
+	Build
+};
 };
 
 namespace QueueSetting {
-	enum Enum {
-		Overwrite,
-		Front,
-		Back
-	};
+enum Enum {
+	Overwrite,
+	Front,
+	Back
+};
 };
 
 
 namespace TerrainType {
-	enum Enum {
-		Invalid,
-		Any,
-		NotWater,
-		Grass,
-		Water,
-		Road,
-		num
-	};
+enum Enum {
+	Invalid,
+	Any,
+	NotWater,
+	Grass,
+	Water,
+	Road,
+	num
+};
 };
 
